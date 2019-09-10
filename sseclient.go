d1094337c01e09ec1aa4caf82f3792422e477a29
Peter Hellberg <peter@c7.se>
2019-09-10 18:59:22 +0200

    Extract unexported loop function

--- sseclient.go
+++ sseclient.go
@@ -17,78 +17,80 @@ type Event struct {
 }
 
 // OpenURL opens a connection to a stream of server sent events
-func OpenURL(rawurl string) (events chan Event, err error) {
+func OpenURL(rawurl string) (chan Event, error) {
 	resp, err := get(rawurl)
 	if err != nil {
 		return nil, err
 	}
 
-	events = make(chan Event)
+	events := make(chan Event)
+	reader := bufio.NewReader(resp.Body)
+
+	go loop(reader, events)
+
+	return events, nil
+}
+
+func loop(reader *bufio.Reader, events chan Event) {
+	ev := Event{}
 
 	var buf bytes.Buffer
 
-	go func() {
-		ev := Event{}
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error during resp.Body read:%s\n", err)
 
-		reader := bufio.NewReader(resp.Body)
-
-		for {
-			line, err := reader.ReadBytes('\n')
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error during resp.Body read:%s\n", err)
-
-				close(events)
-			}
-
-			switch {
-			// OK line
-			case hasPrefix(line, ":ok"):
-				// Do nothing
-
-			// id of event
-			case hasPrefix(line, "id: "):
-				ev.ID = string(line[4:])
-			case hasPrefix(line, "id:"):
-				ev.ID = string(line[3:])
-
-			// name of event
-			case hasPrefix(line, "event: "):
-				ev.Name = string(line[7 : len(line)-1])
-			case hasPrefix(line, "event:"):
-				ev.Name = string(line[6 : len(line)-1])
-
-			// event data
-			case hasPrefix(line, "data: "):
-				buf.Write(line[6:])
-			case hasPrefix(line, "data:"):
-				buf.Write(line[5:])
-
-			// end of event
-			case bytes.Equal(line, []byte("\n")):
-				b := buf.Bytes()
-
-				if hasPrefix(b, "{") {
-					var data map[string]interface{}
-
-					err := json.Unmarshal(b, &data)
-
-					if err == nil {
-						ev.Data = data
-						buf.Reset()
-						events <- ev
-						ev = Event{}
-					}
-				}
-
-			default:
-				fmt.Fprintf(os.Stderr, "Error: len:%d\n%s", len(line), line)
-
-				close(events)
-			}
+			close(events)
 		}
-	}()
 
-	return events, nil
+		switch {
+		case hasPrefix(line, ":"):
+			// Comment, do nothing
+		case hasPrefix(line, "retry:"):
+			// Retry, do nothing for now
+
+		// id of event
+		case hasPrefix(line, "id: "):
+			ev.ID = string(line[4:])
+		case hasPrefix(line, "id:"):
+			ev.ID = string(line[3:])
+
+		// name of event
+		case hasPrefix(line, "event: "):
+			ev.Name = string(line[7 : len(line)-1])
+		case hasPrefix(line, "event:"):
+			ev.Name = string(line[6 : len(line)-1])
+
+		// event data
+		case hasPrefix(line, "data: "):
+			buf.Write(line[6:])
+		case hasPrefix(line, "data:"):
+			buf.Write(line[5:])
+
+		// end of event
+		case bytes.Equal(line, []byte("\n")):
+			b := buf.Bytes()
+
+			if hasPrefix(b, "{") {
+				var data map[string]interface{}
+
+				err := json.Unmarshal(b, &data)
+
+				if err == nil {
+					ev.Data = data
+					buf.Reset()
+					events <- ev
+					ev = Event{}
+				}
+			}
+
+		default:
+			fmt.Fprintf(os.Stderr, "Error: len:%d\n%s", len(line), line)
+
+			close(events)
+		}
+	}
 }
 
 func get(rawurl string) (*http.Response, error) {
